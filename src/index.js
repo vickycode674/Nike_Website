ea5613e36047294a1c51e3819cf873363f0a0126
chviveksai <chvivek674@gmail.com>
2024-05-18 22:00:01 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import Footer from "./sections/Footer";
+import CustomerReviews from "./sections/CustomerReviews";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+
+export{
+    Hero,PopularProducts,Footer,CustomerReviews,Services,SpecialOffer,Subscribe,SuperQuality
+}
